b3b715eb3fef5213c0ef67b991c6399831cc65cb
Ubuntu <ubuntu@ip-172-31-24-11.us-east-2.compute.internal>
2020-06-22 15:34:42 +0000

    configured adding msgs via chatroom form

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,18 @@
 class MessagesController < ApplicationController
+    def new
+        @message = Message.new
+    end
+    
+    def create
+        @message = Message.new(msg_params)
+        @message.user_id = current_user.id
+        @message.save
+        redirect_to root_path
+    end
+    
+    private
+    
+    def msg_params
+        params.require(:message).permit(:comment)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :first_name, presence: true
+  def full_name
+   "#{first_name} #{ last_name}"
+  end
 end
